494af69205a334a3907b66d69c7765bb7e4d0726
Babek Aliyev <baliyev18013@ada.edu.az>
2025-03-02 15:14:06 +0400

    creating table status for cars table

--- Postgres.sql
+++ Postgres.sql
@@ -4,7 +4,8 @@ id serial primary key,
 fullname varchar(25),
 email varchar(50) unique,
 phone varchar(20),
-licenseNum varchar(20) foreign key references cars(licenseNum)
+license_num varchar(20),
+foreign key (license_num) references cars(license_num)
 );
 
 --Creating table Cars
@@ -13,9 +14,15 @@ id serial primary key,
 brand varchar(20),
 model varchar(20),
 year int,
-licenseNum varchar(20) unique primary key,
-status varchar(20) foreign key references car_status(status) 
+license_num varchar(20) unique,
+status varchar(20),
+foreign key (status) references car_status(status) 
 );
 
+-- Creating table car_status
+create table car_status(
+id int primary key,
+status varchar(20) unique
+);
 
 
