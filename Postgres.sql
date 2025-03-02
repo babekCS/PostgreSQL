1115631d26372e8c8299fa61f9b1f37255564035
Babek Aliyev <baliyev18013@ada.edu.az>
2025-03-02 15:04:35 +0400

    Creating second table

--- Postgres.sql
+++ Postgres.sql
@@ -1,7 +1,21 @@
+--Creating table Users (drivers)
 create table users(
 id serial primary key,
 fullname varchar(25),
 email varchar(50) unique,
 phone varchar(20),
-licenseNum varchar(20) unique
+licenseNum varchar(20) foreign key references cars(licenseNum)
 );
+
+--Creating table Cars
+create table cars(
+id serial primary key,
+brand varchar(20),
+model varchar(20),
+year int,
+licenseNum varchar(20) unique primary key,
+status varchar(20) foreign key references car_status(status) 
+);
+
+
+
